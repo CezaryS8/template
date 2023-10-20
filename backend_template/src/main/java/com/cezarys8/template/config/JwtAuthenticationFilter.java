ef66d0cd0e73ee16370a5a4becb9f093d898c6ed
cezarys8 <czarek.skorupski2001@gmail.com>
2023-10-20 16:35:38 +0200

    implement jwt service

---
+++ backend_template/src/main/java/com/cezarys8/template/config/JwtAuthenticationFilter.java
@@ -0,0 +1,36 @@
+package com.cezarys8.template.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+// dzięki OncePerRequestFilter kazdy request przechodzi przez tą klase
+@Component
+@RequiredArgsConstructor // utworzy konstruktor z wszystkimi polami "final"
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String userEmail;
+
+        if(authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+        }
+        jwt = authHeader.substring(7);
+        userEmail = jwtService.extractUsername(jwt);
+    }
+}
